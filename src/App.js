8925d70e7d4dce213320d92e9a148cbe97f8d402
MatheusHBitencourt <matheus.svsg@gmail.com>
2023-07-09 23:40:04 -0300

    feat: root commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {useEffect, useState} from "react";
 
-function App() {
+const App = () => {
+  const [segundo, setSegundo] = useState(null);
+  const [minuto, setMinuto] = useState(null);
+  const [hora, setHora] = useState(null);
+  const [isEnabled, setIsEnabled] = useState(true);
+  const [isPaused, setIsPaused] = useState(false);
+  const [isStart, setIsStart] = useState(true);
+
+  function showTimer() {
+    if (segundo !== null) {
+      setTimeout(startTimer, 1000);
+    }
+
+    const horario = [segundo, minuto, hora];
+
+    return horario;
+  }
+
+  function startTimer() {
+    setIsEnabled(false);
+    setIsStart(false);
+
+    if (!isPaused) {
+      if (segundo !== null) {
+        setSegundo(segundo + 1);
+      } else {
+        setSegundo(0);
+      }
+      if (segundo > 58) {
+        setMinuto(minuto + 1);
+        setSegundo(0);
+      }
+      if (minuto > 58) {
+        setHora(hora + 1);
+        setMinuto(0);
+      }
+    }
+  }
+
+  function pauseTimer() {
+    setIsPaused(true);
+  }
+
+  function resumeTimer() {
+    setIsPaused(false);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {showTimer()[2] < 10
+            ? `0${showTimer()[2] !== null ? showTimer()[2] : `0`}`
+            : showTimer()[2]}
+          :
+          {showTimer()[1] < 10
+            ? `0${showTimer()[1] !== null ? showTimer()[1] : `0`}`
+            : showTimer()[1]}
+          :
+          {showTimer()[0] < 10
+            ? `0${showTimer()[0] !== null ? showTimer()[0] : `0`}`
+            : showTimer()[0]}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!isPaused && isStart && (
+          <button onClick={startTimer} disabled={!isEnabled}>
+            Iniciar
+          </button>
+        )}
+        {!isPaused && !isStart && <button onClick={pauseTimer}>Pausar</button>}
+        {isPaused && <button onClick={resumeTimer}>Retomar</button>}
       </header>
     </div>
   );
-}
+};
 
 export default App;
